ad9f074697fac5114d8d94a6ede418954cc62b43
thanhvu2901 <vcthanh2901@gmail.com>
2022-03-05 20:28:58 +0700

    config fb & preload

---
+++ imposter/src/authencation/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBkN6oOdqv-p3Vz2816bD9NUIXOpqZDYsw",
+    authDomain: "imposter-82056.firebaseapp.com",
+    databaseURL: "https://imposter-82056-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "imposter-82056",
+    storageBucket: "imposter-82056.appspot.com",
+    messagingSenderId: "984246597234",
+    appId: "1:984246597234:web:31ba7cfaae378b03ae74eb",
+    measurementId: "G-7QLRDCQ306"
+};
+
+export default firebaseConfig;
--- imposter/src/index.js
+++ imposter/src/index.js
@@ -1,35 +1,23 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
 
-class MyGame extends Phaser.Scene {
-    constructor() {
-        super();
-    }
 
-    preload() {
-        this.load.image('logo', logoImg);
-    }
-
-    create() {
-        const logo = this.add.image(400, 150, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-}
+import Preloader from '../src/sence/preload'
+import SceneKeys from '../src/consts/SceneKeys'
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
-};
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Preloader
+}
 
 const game = new Phaser.Game(config);
+
+//game.scene.add(SceneKeys.Preloader, Preloader)
+
+export default game
