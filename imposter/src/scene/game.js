4648681a1638a06ad629fb1a9ae7ed92dc162216
Hoang Phuc <thienloc1220@gmail.com>
2022-03-22 01:25:06 +0700

    Implement boundary of map (not yet finished)

--- imposter/src/scene/game.js
+++ imposter/src/scene/game.js
@@ -3,6 +3,7 @@ import tileImg from "../assets/img/theSkeld.png";
 import theskeld from "../assets/tilemaps/theskeld.json";
 import playerpng from "../assets/player/player_sprite/player_base.png";
 import playerjson from "../assets/player/player_sprite/player_base.json";
+import { debugDraw } from "../scene/debugDraw";
 
 import {
   PLAYER_SPRITE_WIDTH,
@@ -22,22 +23,17 @@ class Game extends Phaser.Scene {
   preload() {
     this.load.image("tiles", tileImg);
     this.load.tilemapTiledJSON("tilemap", theskeld);
-    // this.load.spritesheet("player", playerSprite, {
-    //     frameWidth: PLAYER_SPRITE_WIDTH,
-    //     frameHeight: PLAYER_SPRITE_HEIGHT,
-    // });
-    // this.load.spritesheet("idle", idle, {
-    //     frameWidth: PLAYER_SPRITE_WIDTH,
-    //     frameHeight: PLAYER_SPRITE_HEIGHT,
-    // });
     this.load.atlas("playerbase", playerpng, playerjson);
   }
 
   create() {
-    // const ship = this.add.image(0, 0, "ship");
     const ship = this.make.tilemap({ key: "tilemap" });
     const tileset = ship.addTilesetImage("theSkeld", "tiles");
-    ship.createLayer("Background", tileset);
+
+    const ship_tileset = ship.createLayer("Background", tileset);
+    ship_tileset.setCollisionByProperty({ collides: true });
+
+    debugDraw(ship_tileset, this);
 
     player = this.physics.add.sprite(128, 128, "playerbase", "idle.png");
     cursors = this.input.keyboard.createCursorKeys();
@@ -71,25 +67,6 @@ class Game extends Phaser.Scene {
       frameRate: 24,
     });
 
-    // player.anims.play("player-dead");
-
-    // player.sprite = this.add.sprite(PLAYER_START_X, PLAYER_START_Y, "player");
-    // player.sprite.displayHeight = PLAYER_HEIGHT;
-    // player.sprite.displayWidth = PLAYER_WIDTH;
-
-    // this.anims.create({
-    //     key: "running",
-    //     frames: this.anims.generateFrameNumbers("player"),
-    //     frameRate: 24,
-    //     reapeat: -1,
-    // });
-    // this.anims.create({
-    //     key: "idle",
-    //     frames: this.anims.generateFrameNumbers("idle"),
-    //     frameRate: 1,
-    //     reapeat: 0,
-    // });
-
     this.input.keyboard.on("keydown", (e) => {
       if (!pressedKeys.includes(e.code)) {
         pressedKeys.push(e.code);
@@ -98,14 +75,22 @@ class Game extends Phaser.Scene {
     this.input.keyboard.on("keyup", (e) => {
       pressedKeys = pressedKeys.filter((key) => key !== e.code);
     });
+
+    this.physics.add.collider(player, ship_tileset);
+
+    this.cameras.main.startFollow(player, true);
   }
 
   update() {
     // this.scene.scene.cameras.main.centerOn(player.sprite.x, player.sprite.y);
-    this.cameras.main.startFollow(player);
 
     player.setVelocity(0);
-    if (!cursors.left.isDown && !cursors.right.isDown && !cursors.up.isDown && !cursors.down.isDown) {
+    if (
+      !cursors.left.isDown &&
+      !cursors.right.isDown &&
+      !cursors.up.isDown &&
+      !cursors.down.isDown
+    ) {
       player.anims.play("player-idle");
     }
 
