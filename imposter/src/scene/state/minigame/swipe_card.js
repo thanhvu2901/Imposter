4e5f3ad95216d9525d0b10431ac8a762b60d8fb3
Hoang Phuc <thienloc1220@gmail.com>
2022-04-08 13:36:56 +0700

    add asset

--- imposter/src/scene/state/minigame/swipe_card.js
+++ imposter/src/scene/state/minigame/swipe_card.js
@@ -6,7 +6,7 @@ import admin_textBar from "../../../assets/tasks/Swipe Card/admin_textBar.png";
 import admin_Wallet from "../../../assets/tasks/Swipe Card/admin_Wallet.png";
 import admin_walletFront from "../../../assets/tasks/Swipe Card/admin_walletFront.png";
 
-var board, card, mini_card, card_holder, wallet, textBar;
+var board, card, mini_card, wallet_front, wallet, textBar;
 var container;
 
 class SwipeCard extends Phaser.Scene {
@@ -21,12 +21,19 @@ class SwipeCard extends Phaser.Scene {
   create() {
     board = this.add.image(512, 384, "admin_BG");
     // card = this.add.image(512, 384, "admin_Card");
-    mini_card = this.add.image(512, 384, "admin_Card");
-    mini_card.setScale(0.5, 0.5);
-    card_holder = this.add.image(525, 384, "admin_walletFront");
-    container = this.add.container(10, 20, [card_holder, mini_card]);
-    container.setSize(card_holder.width, card_holder.height);
-    wallet = this.add.image(512, 550, "admin_Wallet");
+    mini_card = this.add.image(-105, 20, "admin_Card");
+    mini_card.setScale(0.75, 0.75);
+    mini_card.setDepth(1);
+    wallet = this.add.image(0, 0, "admin_Wallet");
+    wallet_front = this.add.image(-105, 40, "admin_walletFront");
+    wallet_front.setDepth(2);
+    container = this.add.container(512, 550, [wallet, mini_card, wallet_front]);
+    container.setInteractive(new Phaser.Geom.Rectangle(-220, -50, mini_card.width, mini_card.height), Phaser.Geom.Rectangle.Contains);
+    this.input.enableDebug(container, 0xff00ff);
+    container.on("pointerdown", function() {
+      console.log("pointerdown");
+    })
+    // container.on("pointerup", function() {})
     
   }
   update() {}
