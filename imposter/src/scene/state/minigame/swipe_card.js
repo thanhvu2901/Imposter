de9c7c04c98ed6b381b5423816d4731c4e7f6f4f
Hoang Phuc <thienloc1220@gmail.com>
2022-04-09 00:10:20 +0700

    Commit code for testing

--- imposter/src/scene/state/minigame/swipe_card.js
+++ imposter/src/scene/state/minigame/swipe_card.js
@@ -2,28 +2,29 @@ import Phaser from "phaser";
 import admin_BG from "../../../assets/tasks/Swipe Card/admin_BG.png";
 import admin_Card from "../../../assets/tasks/Swipe Card/admin_Card.png";
 import admin_sliderBottom from "../../../assets/tasks/Swipe Card/admin_sliderBottom.png";
+import admin_sliderTop from "../../../assets/tasks/Swipe Card/admin_sliderTop.png";
 import admin_textBar from "../../../assets/tasks/Swipe Card/admin_textBar.png";
 import admin_Wallet from "../../../assets/tasks/Swipe Card/admin_Wallet.png";
 import admin_walletFront from "../../../assets/tasks/Swipe Card/admin_walletFront.png";
-<<<<<<< HEAD
-
-var board, card, mini_card, wallet_front, wallet, textBar;
-=======
 let text;
-var board, card, mini_card, card_holder, wallet, textBar;
->>>>>>> 2884d65be36e2cb3c3e119a233b5d05d3de11824
-var container;
+var board, mini_card, card_holder, wallet, textBar, slider_bot, slider_top;
 
-var target = new Phaser.Math.Vector2();
-let leave=false;
-let move
-let step=0
-let scale=0.5
+var target1 = new Phaser.Math.Vector2();
+var target2 = new Phaser.Math.Vector2();
+var start = new Phaser.Math.Vector2();
+var distance1, distance2;
+let leave = false;
+let move;
+let outRanged = false;
+let step = 0;
+let scale = 0.75;
+let finished = false;
 class SwipeCard extends Phaser.Scene {
   preload() {
     this.load.image("admin_BG", admin_BG);
     this.load.image("admin_Card", admin_Card);
     this.load.image("admin_sliderBottom", admin_sliderBottom);
+    this.load.image("admin_sliderTop", admin_sliderTop);
     this.load.image("admin_textBar", admin_textBar);
     this.load.image("admin_Wallet", admin_Wallet);
     this.load.image("admin_walletFront", admin_walletFront);
@@ -31,74 +32,103 @@ class SwipeCard extends Phaser.Scene {
   create() {
     board = this.add.image(512, 384, "admin_BG");
     // card = this.add.image(512, 384, "admin_Card");
-<<<<<<< HEAD
-    mini_card = this.add.image(-105, 20, "admin_Card");
+    mini_card = this.physics.add.image(400, 570, "admin_Card").setInteractive();
     mini_card.setScale(0.75, 0.75);
     mini_card.setDepth(1);
-    wallet = this.add.image(0, 0, "admin_Wallet");
-    wallet_front = this.add.image(-105, 40, "admin_walletFront");
-    wallet_front.setDepth(2);
-    container = this.add.container(512, 550, [wallet, mini_card, wallet_front]);
-    container.setInteractive(new Phaser.Geom.Rectangle(-220, -50, mini_card.width, mini_card.height), Phaser.Geom.Rectangle.Contains);
-    this.input.enableDebug(container, 0xff00ff);
-    container.on("pointerdown", function() {
-      console.log("pointerdown");
-    })
-    // container.on("pointerup", function() {})
-=======
-    mini_card = this.physics.add.image(512, 384, "admin_Card").setInteractive();
-    mini_card.setScale(0.5, 0.5);
-    card_holder = this.add.image(525, 384, "admin_walletFront");
-    // container = this.add.container(10, 20, [card_holder, mini_card]);
-    // container.setSize(card_holder.width, card_holder.height);
+    start.x = mini_card.x;
+    start.y = mini_card.y;
     wallet = this.add.image(512, 550, "admin_Wallet");
->>>>>>> 2884d65be36e2cb3c3e119a233b5d05d3de11824
-    
+    card_holder = this.add.image(405, 588, "admin_walletFront");
+    card_holder.setDepth(2);
+    slider_top = this.add.image(512, 220, "admin_sliderTop");
+    slider_top.setDepth(2);
+    slider_bot = this.add.image(512, 325, "admin_sliderBottom");
+    this.input.on(
+      "pointerdown",
+      function (pointer) {
+        if (mini_card.x != 800 && mini_card.y != 312 && leave == false) {
+          target1.x = 280;
+          target1.y = 312;
+          target2.x = 750;
+          target2.y = 312;
+          leave = true;
+          // Move at 300 px/s:
+          move = this.physics.moveToObject(mini_card, target1, 300);
 
-    this.input.on('pointerdown', function (pointer) {
-if(mini_card.x!=402&& mini_card.y!=256&&leave==false){
-      target.x = 402;
-      target.y = 256;
-      leave=true
-      // Move at 200 px/s:
-   move=   this.physics.moveToObject(mini_card, target, 200);
-  
-     
-      this.input.setDraggable(mini_card);
+          this.input.setDraggable(mini_card);
+        }
+      },
+      this
+    );
+    text = this.add
+      .text(10, 10, "Cursors to move", {
+        font: "16px Courier",
+        fill: "#00ff00",
+      })
+      .setScrollFactor(0);
 
-}
-  }, this);
-  text = this.add.text(10, 10, 'Cursors to move', { font: '16px Courier', fill: '#00ff00' }).setScrollFactor(0);
+    this.input.on("drag", function (pointer, gameObject, dragX, dragY) {
+      gameObject.x = dragX;
+      console.log(gameObject.speed)
+    });
 
-  this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-
-    gameObject.x = dragX;
-   // gameObject.y = dragY;
-
-});
   }
-  
+
   update() {
-   step+=1
-   //console.log(leave)
-   if(leave===true&&scale<=1){
-     mini_card.setScale(scale)
-     scale+=0.009
-   }
-  //  console.log(mini_card.getScale())
-    text.setText([
-      'screen x: ' + this.input.x,
-      'screen y: ' + this.input.y,
-  ]);
-  var distance = Phaser.Math.Distance.Between(mini_card.x, mini_card.y, target.x, target.y);
+    step += 1;
+    //console.log(leave)
+    if (leave === true && scale <= 1) {
+      mini_card.setScale(scale);
+      scale += 0.0075;
+    }
+    //  console.log(mini_card.getScale())
+    distance1 = Phaser.Math.Distance.Between(
+      mini_card.x,
+      mini_card.y,
+      target1.x,
+      target1.y
+    );
+    distance2 = Phaser.Math.Distance.Between(
+      mini_card.x,
+      mini_card.y,
+      target2.x,
+      target2.y
+    );
+    if (mini_card.x >= target2.x && leave) {
+      mini_card.x = target2.x;
+    }
+    if (mini_card.x <= target1.x && leave) {
+      mini_card.x = target1.x;
+    }
+    if (mini_card.x == target2.x) {
+      finished = true;
+    }
+    if (finished) {
+      move = this.physics.moveToObject(mini_card, start, 300);
+    }
+    if (mini_card.x == start.x && mini_card.y == start.y && finished) {
+      leave = false;
+      finished = false;
+      move.stop();
+      scale = 0.75;
+    }
 
-  if (mini_card.body.speed > 0)
-  {
-      if (distance < 4)
-      {
-          mini_card.body.reset(target.x, target.y);
+    if (mini_card.body.speed > 0) {
+      if (distance1 < 4) {
+        mini_card.body.reset(target1.x, target1.y);
       }
-  }
+    }
+    if (outRanged) {
+      mini_card.body.reset(target1.x, target1.y);
+      outRanged = false;
+    }
+
+    text.setText([
+      "screen x: " + this.input.x,
+      "screen y: " + this.input.y,
+      "card speed: " + mini_card.body.speed,
+      "card x: " + mini_card.x,
+    ]);
   }
 }
 
