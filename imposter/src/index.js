283fedfb2eb94597c3d25c40a5cacef00d6209be
Hoang Phuc <thienloc1220@gmail.com>
2022-04-25 20:35:25 +0700

    Add background with boundary for Lobby room

--- imposter/src/index.js
+++ imposter/src/index.js
@@ -6,12 +6,6 @@ import TheSkeld from "./scene/state/ingame/theskeld.js";
 import ChartCourse from "./scene/state/ingame/chartcourse.js";
 import SwipeCard from "./scene/state/minigame/swipe_card.js";
 import Game from './scene/game.js'
-<<<<<<< HEAD
-import FuelEngine from "./scene/state/ingame/fuelengine.js";
-import FuelEngine_1 from "./scene/state/ingame/fuelengine_1.js";
-import UnlockManifolds from "./scene/state/ingame/unlockmanifolds.js";
-import UploadData from "./scene/state/ingame/uploaddata.js";
-=======
 import Options from "./scene/state/inmenu/options.js";
 import Login from "./scene/login.js";
 import SubMenu from "./scene/submenu.js";
@@ -26,7 +20,6 @@ import StabilizeSteering from "./scene/state/minigame/stabilize_steering.js";
 import StartReactor from "./scene/state/minigame/start_reactor.js";
 import JoinGame from "./scene/joinGame.js";
 import waitingRoom from "./scene/waitingRoom.js";
->>>>>>> 974eb27bb870dfd73c42056665d758badf29b6b5
 var preloader = new Preloader();
 
 const config = {
@@ -54,13 +47,6 @@ game.scene.add("preloader", preloader);
 game.scene.add("menu", MainMenuScene);
 game.scene.add("theskeld", TheSkeld);
 game.scene.add('game', Game);
-<<<<<<< HEAD
-game.scene.add('Course',ChartCourse)
-game.scene.add('Fuel',FuelEngine)
-game.scene.add('Fuel_1',FuelEngine_1)
-game.scene.add('Unlock',UnlockManifolds)
-game.scene.add('Upload',UploadData)
-=======
 
 game.scene.add('options', Options);
 game.scene.add('login', Login);
@@ -84,19 +70,16 @@ game.scene.add('stabilizeSteering', StabilizeSteering)
 game.scene.add('startReactor', StartReactor)
 game.scene.add('joinGame', JoinGame)
 game.scene.add('waitingRoom', waitingRoom)
->>>>>>> 974eb27bb870dfd73c42056665d758badf29b6b5
 // start title
 
 //game.scene.start("preloader");
 // game.scene.start("theskeld");
 //game.scene.start('menu');
-game.scene.start('game');
+// game.scene.start('game');
 //game.scene.start('login');
 // game.scene.start('Upload');
 
-<<<<<<< HEAD
-=======
-game.scene.start('preloader');
+// game.scene.start('preloader');
 //game.scene.start('Course');
 // game.scene.start('Course');
 //game.scene.start('CleanO2Filter');
@@ -111,5 +94,4 @@ game.scene.start('preloader');
 // game.scene.start('preloader');
 //game.scene.start("align_engine_output")
 //game.scene.start('joinGame')
-//game.scene.start('waitingRoom')
->>>>>>> 974eb27bb870dfd73c42056665d758badf29b6b5
+game.scene.start('waitingRoom')
--- imposter/src/scene/waitingRoom.js
+++ imposter/src/scene/waitingRoom.js
@@ -1,18 +1,112 @@
 import Phaser from "phaser";
-import dropShip from '../assets/img/Dropship.png'
+import dropShip from "../assets/img/Dropship.png";
+import lobby from "../assets/tilemaps/lobby.json";
+import playerpng from "../assets/player/player_sprite/player_base.png";
+import playerjson from "../assets/player/player_sprite/player_base.json";
+import { PLAYER_SPEED } from "../consts/constants";
+import { debugDraw } from "../scene/debugDraw";
+
+let player;
+let cursors;
+
 export default class waitingRoom extends Phaser.Scene {
-    constructor() {
-        super({
-            key: 'waitingRoom'
-        })
+  constructor() {
+    super({
+      key: "waitingRoom",
+    });
+  }
+
+  preload() {
+    this.load.image("dropShip", dropShip);
+    this.load.tilemapTiledJSON("lobby", lobby);
+    this.load.atlas("playerbase", playerpng, playerjson);
+  }
+
+  create() {
+    const lobby = this.make.tilemap({ key: "lobby" });
+    const tileset = lobby.addTilesetImage("Dropship", "dropShip");
+    const lobby_tileset = lobby.createLayer("Background", tileset);
+    lobby_tileset.setCollisionByProperty({ collides: true });
+    debugDraw(lobby_tileset, this);
+
+    //cursor to direct
+    cursors = this.input.keyboard.createCursorKeys();
+
+    player = this.physics.add.sprite(150, 128, "playerbase", "idle.png");
+
+    // tạo object và gán các thuộc tính
+    this.anims.create({
+      key: "player-idle",
+      frames: [{ key: "playerbase", frame: "idle.png" }],
+    });
+
+    //animation player
+    this.anims.create({
+      key: "player-walk",
+      frames: this.anims.generateFrameNames("playerbase", {
+        start: 1,
+        end: 12,
+        prefix: "Walk",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 16,
+    });
+
+    //player death
+    this.anims.create({
+      key: "player-dead",
+      frames: this.anims.generateFrameNames("playerbase", {
+        start: 1,
+        end: 42,
+        prefix: "Dead",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 24,
+    });
+
+    this.physics.add.collider(player, lobby_tileset);
+
+    this.cameras.main.startFollow(player, true);
+  }
+
+  update() {
+    let playerMoved = false;
+
+    player.setVelocity(0);
+    if (
+      !cursors.left.isDown &&
+      !cursors.right.isDown &&
+      !cursors.up.isDown &&
+      !cursors.down.isDown
+    ) {
+      player.anims.play("player-idle");
     }
 
-    preload() {
-        this.load.image('dropShip', dropShip);
-
+    // when move
+    if (cursors.left.isDown) {
+      player.anims.play("player-walk", true);
+      player.setVelocityX(-PLAYER_SPEED);
+      player.scaleX = -1;
+      player.body.offset.x = 40;
+      playerMoved = true;
+    } else if (cursors.right.isDown) {
+      player.anims.play("player-walk", true);
+      player.setVelocityX(PLAYER_SPEED);
+      player.scaleX = 1;
+      player.body.offset.x = 0;
+      playerMoved = true;
     }
 
-    create() {
-        this.add.image(0, 0, 'dropShip')
+    if (cursors.up.isDown) {
+      player.anims.play("player-walk", true);
+      player.setVelocityY(-PLAYER_SPEED);
+      playerMoved = true;
+    } else if (cursors.down.isDown) {
+      player.anims.play("player-walk", true);
+      player.setVelocityY(PLAYER_SPEED);
+      playerMoved = true;
     }
+  }
 }
