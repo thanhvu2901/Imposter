ada0bbad7c235dfb4d1cccdb2d33165db03c543f
thanhvu2901 <vcthanh2901@gmail.com>
2022-05-16 15:34:53 +0700

    intro

--- imposter/src/scene/state/intro/introCrew.js
+++ imposter/src/scene/state/intro/introCrew.js
@@ -1,7 +1,8 @@
+import { Runner } from "matter";
 import Phaser from "phaser";
 import audio from '../../../assets/audio/intro/Role_Reveal.mp3'
 import crew from '../../../assets/img/intro/intro.jpg'
-
+let isRole;
 class introCrew extends Phaser.Scene {
     constructor() {
         super({
@@ -9,32 +10,59 @@ class introCrew extends Phaser.Scene {
         })
     }
     init(data) {
-        this.isRole = data.isRole
+        this.socket = data.socket;
+        this.textInput = data.textInput;
+        this.numPlayers = data.numPlayers;
+        this.idPlayers = data.idPlayers;
+
+        //  this.isRole = data.isRole
     }
     //*********lấy socket định danh role và thông báo tới */
     //IMPOSTER?
     preload() {
         this.load.image('crew', crew)
         this.load.audio('intro', audio)
+        this.socket.emit("whatRole", (this.textInput));
+        this.socket.on("roleIs", (role) => {
+            //console.log(role);
+            // is imposterr
+            isRole = role;
+        });
     }
     create() {
-        if (this.isRole == 1) {
+
+        if (isRole == 1) {
             var sprite = this.add.image(530, 350, 'crew').setScale(0.6).setAlpha(0);
             let text = this.add.text(350, 230, 'YOU ARE CREWMATE!', { fontSize: '40px' }).setAlpha(0)
             //chạy intro logo
             let audio = this.sound.add('intro', {})
             setTimeout(audio.play(), 1000)
             var intro = this.tweens.add({
-                targets: [sprite, text, audio],
+                targets: [sprite, text],
                 alpha: 1,
-                yoyo: false,
                 duration: 5000,
                 ease: 'Power1',
-                delay: 1000
-            });
-        }
-        //    setTimeout(this.scene.stop('introCrew'), 10000)
 
+                completeDelay: 2000,
+                // onComplete: console.log('done'),
+                // callbackScope: this.scene.stop('introCrew')
+
+            });
+            intro.complete(2000)
+            intro.callbacks
+        }
+
+
+
+        else {
+            // imposter == 1
+        }
+        //setTimeout(console.log('after done'), 6000)
+        //);
+    }
+    Run() {
+        this.scene.launch('game', { socket: this.socket, textInput: this.textInput, numPlayers: this.numPlayers, idPlayers: this.idPlayers })
+        this.scene.stop('introCrew');
     }
 }
 
